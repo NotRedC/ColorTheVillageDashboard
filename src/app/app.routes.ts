f689025d936537399fb384a43d30ad5ad40cb541
NotRedC <redcharizard1@gmail.com>
2025-06-01 11:09:01 +0300

    Commit 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { Layout } from './layout/layout';
+import { Volunteers } from './volunteers/volunteers';
+import { Events } from './events/events';
+import { Donations } from './donations/donations';
+import { Tasks } from './tasks/tasks';
+import { Dashboard } from './dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Layout,
+    children: [
+      { path: '', component: Dashboard },
+      { path: 'volunteers', component: Volunteers },
+      { path: 'events', component: Events },
+      { path: 'donations', component: Donations },
+      { path: 'tasks', component: Tasks }
+    ]
+  }
+];
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
-bootstrapApplication(App, appConfig)
+bootstrapApplication(App, { 
+  ...appConfig,
+  providers: [
+    ...(appConfig.providers ?? []),
+    provideNativeDateAdapter()
+  ]
+})
   .catch((err) => console.error(err));
